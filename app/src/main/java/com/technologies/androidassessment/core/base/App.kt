a612c5a43f5ee9a70cba2dd46a025a80dd39b0c9
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-09-12 20:05:17 +0800

    :zap: setup base classes :zap: add extension for LiveData

---
+++ app/src/main/java/com/technologies/androidassessment/core/base/App.kt
@@ -0,0 +1,11 @@
+package com.technologies.androidassessment.core.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
